f2bbffb253ef20fd5e295157e3813683ee8c3ad8
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2025-11-23 04:48:47 -0500

    increased performance of upload

--- GameCollectionManager.Server/Services/DB_Services.cs
+++ GameCollectionManager.Server/Services/DB_Services.cs
@@ -15,23 +15,26 @@ namespace GameCollectionManagerAPI.Services
         List<GameDAO> gameList = new List<GameDAO>();
         private ILog log = LogManager.GetLogger(typeof(Program));
         private readonly IConfiguration _configuration;
-        private DataContext _context;
+        private readonly IServiceScopeFactory _scopeFactory;
 
-        public DB_Services(IConfiguration configuration, DataContext dataContext)
+        public DB_Services(IConfiguration configuration, IServiceScopeFactory scopeFactory)
         {
             this._configuration = configuration;
-            _context = dataContext;
+            this._scopeFactory = scopeFactory;
         }
 
         public async Task<List<GameDAO>> GetGamesAsync(string user)
         {
             try
             {
+                using var scope = _scopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                
                 if (user == null)
                 {
                     user = "NoUser";
                 }
-                return await _context.Games.Where(c => c.owner == user).ToListAsync();
+                return await context.Games.Where(c => c.owner == user).ToListAsync();
             }
             catch (Exception ex)
             {
@@ -39,15 +42,19 @@ namespace GameCollectionManagerAPI.Services
                 return new List<GameDAO>();
             }
         }
+        
         public async Task<List<GameDAO>> GetQueueAsync(string user)
         {
             try
             {
+                using var scope = _scopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                
                 if (user == null)
                 {
                     user = "NoUser";
                 }
-                return await _context.Games.Where(c => c.owner == user && c.queuepos != null).OrderBy(x => x.queuepos).ToListAsync();
+                return await context.Games.Where(c => c.owner == user && c.queuepos != null).OrderBy(x => x.queuepos).ToListAsync();
             }
             catch (Exception ex)
             {
@@ -55,12 +62,16 @@ namespace GameCollectionManagerAPI.Services
                 return new List<GameDAO>();
             }
         }
+        
         public async Task CreateTable(GameDAO game)
         {
             try
             {
-                _context.Add(game);
-                _context.SaveChanges();
+                using var scope = _scopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                
+                context.Add(game);
+                await context.SaveChangesAsync();
             }
             catch(Exception e)
             {
@@ -68,39 +79,52 @@ namespace GameCollectionManagerAPI.Services
                 throw new Exception("Failed to Create New Table");
             }
         }
+        
         public async Task SimpleUpsert(GameDAO game)
         {
-            var gameToUpdate = await _context.Games.Where(c => c.id == game.id && c.owner == game.owner).FirstOrDefaultAsync();
+            using var scope = _scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+            
+            var gameToUpdate = await context.Games.Where(c => c.id == game.id && c.owner == game.owner).FirstOrDefaultAsync();
             if(gameToUpdate == null)
             {
-                await CreateTable(game);
+                context.Add(game);
+                await context.SaveChangesAsync();
                 return;
             }
             else
             {
-                _context.Entry(gameToUpdate).CurrentValues.SetValues(game);
-                await _context.SaveChangesAsync();
+                context.Entry(gameToUpdate).CurrentValues.SetValues(game);
+                await context.SaveChangesAsync();
             }
         }
+        
         public async Task SimpleDelete(GameDAO game)
         {
-            var gameToRemove = await _context.Games.Where(c => c.id == game.id && c.owner == game.owner).FirstOrDefaultAsync();
+            using var scope = _scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+            
+            var gameToRemove = await context.Games.Where(c => c.id == game.id && c.owner == game.owner).FirstOrDefaultAsync();
             if(gameToRemove == null)
             {
                 throw new Exception("No Game Found");
             }
             else
             {
-                _context.Games.Remove(gameToRemove);
-                await _context.SaveChangesAsync();
+                context.Games.Remove(gameToRemove);
+                await context.SaveChangesAsync();
             }
         }
+        
         public async Task AddToQueueAsync(GameDAO game)
         {
             try
             {
+                using var scope = _scopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                
                 // Get the current highest queue position
-                var maxQueuePos = await _context.Games
+                var maxQueuePos = await context.Games
                     .Where(g => g.owner == game.owner && g.queuepos.HasValue)
                     .MaxAsync(g => (int?)g.queuepos) ?? 0;
 
@@ -108,7 +132,16 @@ namespace GameCollectionManagerAPI.Services
                 game.queuepos = maxQueuePos + 1;
 
                 // Update the game in the database
-                await SimpleUpsert(game);
+                var gameToUpdate = await context.Games.Where(c => c.id == game.id && c.owner == game.owner).FirstOrDefaultAsync();
+                if(gameToUpdate == null)
+                {
+                    context.Add(game);
+                }
+                else
+                {
+                    context.Entry(gameToUpdate).CurrentValues.SetValues(game);
+                }
+                await context.SaveChangesAsync();
             }
             catch (Exception e)
             {
@@ -116,11 +149,15 @@ namespace GameCollectionManagerAPI.Services
                 throw new Exception("Failed to Add Game to Queue");
             }
         }
+        
         public async Task RemoveFromQueueAsync(GameDAO game)
         {
             try
             {
-                var gameToUpdate = await _context.Games
+                using var scope = _scopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+                
+                var gameToUpdate = await context.Games
                     .Where(c => c.id == game.id && c.owner == game.owner)
                     .FirstOrDefaultAsync();
                 if (gameToUpdate == null)
@@ -130,16 +167,17 @@ namespace GameCollectionManagerAPI.Services
                 var removedQueuePos = gameToUpdate.queuepos;
                 // Remove the game from the queue
                 gameToUpdate.queuepos = null;
-                await _context.SaveChangesAsync();
+                await context.SaveChangesAsync();
+                
                 // Decrement the queue positions of games that were after the removed game
-                var gamesToUpdate = await _context.Games
+                var gamesToUpdate = await context.Games
                     .Where(c => c.owner == game.owner && c.queuepos > removedQueuePos)
                     .ToListAsync();
                 foreach (var g in gamesToUpdate)
                 {
                     g.queuepos -= 1;
                 }
-                await _context.SaveChangesAsync();
+                await context.SaveChangesAsync();
             }
             catch (Exception e)
             {
